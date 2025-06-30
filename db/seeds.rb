5c9a07ffc1055fb1e5f8db2af59b4bfc3737ada3
hpeters <hpeters3@academic.rrc.ca>
2025-06-30 18:23:50 -0500

    Faker Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+i = 0
+j = 1
+
+while i < 676 do
+  Product.create(id: j, title: Faker::Games::Pokemon.name, price: Faker::Commerce.price(range: 0.25..99.9), stock_quantity: Faker::Number.number(digits: 2))
+  i += 1
+  j += 1
+end
