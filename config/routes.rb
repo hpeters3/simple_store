6ffbea2a926a8a44a738f7bad3598a531f5fd057
hpeters <hpeters3@academic.rrc.ca>
2025-06-30 21:23:48 -0500

    GET request updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # get "product/index"
   get "/products", to: "product#index"
+  get "/products/:id", to: "product#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
