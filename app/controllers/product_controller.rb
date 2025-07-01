56f19cf2479bb3f9268c615ce80426adb01f446b
hpeters <hpeters3@academic.rrc.ca>
2025-07-01 17:53:59 -0500

    CSV Updates

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,6 @@
 class ProductController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.includes(:category).all
   end
   def show
     @product = Product.find(params[:id])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,23 @@
-i = 0
-j = 1
+# i = 0
+# j = 1
+#
+# while i < 676 do
+#  Product.create(id: j, title: Faker::Games::Pokemon.#name, price: Faker::Commerce.price(range: 0.25..99.#9), stock_quantity: Faker::Number.number(digits: #2))
+#  i += 1
+#  j += 1
+# end
 
-while i < 676 do
-  Product.create(id: j, title: Faker::Games::Pokemon.name, price: Faker::Commerce.price(range: 0.25..99.9), stock_quantity: Faker::Number.number(digits: 2))
-  i += 1
-  j += 1
+require "csv"
+
+Category.destroy_all
+Product.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+  Product.create(title: product['title'], price: product['price'], stock_quantity: product['stock_quantity'], category_id: category.id)
 end
